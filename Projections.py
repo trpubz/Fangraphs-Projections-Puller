c456ee512a4413f95290a98fdd545f3199fe1d72
trpubz <taylor.pubins@me.com>
2022-04-19 16:36:45 -0700

    initial

---
+++ Projections.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Projections(Enum):
+    DC_RoS = "rfangraphsdc"
+    ZiPS_RoS = "rzips"
+    ATC = "atc"
---
+++ Stats.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class StatGrp(Enum):
+    HIT = 'bat'
+    PIT = 'pit'
